a62376452715020299e64690bc8c5e8cbaeb4bdb
Seth Lewis <97172419+slandonlewis@users.noreply.github.com>
2022-08-22 10:46:40 -0500

    phase 3 and 4

--- Program.cs
+++ Program.cs
@@ -1,8 +1,20 @@
 ﻿using System;
 
 Console.Write("Guess a secret number:");
-int guessedNumber = int.Parse(Console.ReadLine());
 int secretNumber = 42;
-string result = (guessedNumber == secretNumber)
-? "You guessed the secret number!" : "Try again...";
-Console.WriteLine(result);
+
+int attemptsRemaining = 4;
+while (attemptsRemaining > 0)
+{
+    attemptsRemaining--;
+    int guessedNumber = int.Parse(Console.ReadLine());
+    string attemptsMessage = (attemptsRemaining > 0) ? $"{attemptsRemaining} attempts left... " : "GAME OVER";
+    bool correct = (guessedNumber == secretNumber);
+    string result = (guessedNumber == secretNumber)
+    ? "You guessed the secret number!" : attemptsMessage;
+    Console.WriteLine(result);
+    if (correct)
+    {
+        break;
+    }
+};
